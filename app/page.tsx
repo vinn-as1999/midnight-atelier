122e22696a6fbd7f2afd0b3e56a55a7c0bde9f55
vinn-as1999 <vinicyus.alves302@gmail.com>
2026-04-14 07:56:49 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+      <nav>
+        <div>
+          logo
+        </div>
+
+        <ul>
+          <li>about</li>
+          <li>services</li>
+          <li>barbers</li>
+          <li>contact</li>
+        </ul>
+
+        <button>
+          book now
+        </button>
+      </nav>
+
+      {
+        // TELA PRINCIPAL AQUI
+      }
+
+      {
+        // SERVIÇOS AQUI
+      }
+
+      {
+        // BARBEIROS AQUI
+      }
+
+      {
+        // FOOTER
+      }
+    </div>
+  );
+}
